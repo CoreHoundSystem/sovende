73fa2b5bfeb7251834240d1e92d638a420a2efa5
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2025-07-21 19:40:14 -0400

    Update loginRegister.js

--- js/loginRegister.js
+++ js/loginRegister.js
@@ -49,7 +49,7 @@ function getData(x,y,z,c) {
 }
 function emailCheck(x) {
     a = [];
-    for(i=0;i<x;i++) {
+    for(i=0;i<x.length;i++) {
         a.push(x[i][0]);
         console.log(a);
     }
