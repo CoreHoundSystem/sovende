43d9a02c9d3736ec96a3f572ddc77e4741eff47d
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2025-07-21 19:03:05 -0400

    Update loginRegister.js

--- js/loginRegister.js
+++ js/loginRegister.js
@@ -41,6 +41,7 @@ function getData(x,y,z,c) {
         "https://sheets.googleapis.com/v4/spreadsheets/" + x + "/values/" + y + "!" + z + "?key=" + apiKey,
         function(data) {
         console.log(data);
+        console.log($('#email').val());
         console.log(data.values[0]);
         c(data.values);
         }
