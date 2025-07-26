897bf7ba62b9d7751bf0002aa3675b253545ff0d
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2025-07-25 20:12:07 -0400

    Create tos.js

---
+++ js/tos.js
@@ -0,0 +1,12 @@
+function tOSAgree(x) {
+    console.log($(location).search());
+    /*
+    emailId = ??; //get from url?
+    version = ??; //built in
+    acctObj = ??; //get from url?
+    acct = ['https://docs.google.com/forms/d/e/1FAIpQLSet01OUiLbgS9t6YNVqVV6C_KQAdCWS_w5kHiG8O_OYX_RfoA/formResponse?usp=pp_url','&entry.1292056060=','&entry.556119510=','&entry.1334536695='];
+    x = acct[0] + acct[1] + emailId + acct[2] + version + acct[3] + acctObj;
+    $('body').append('<iframe src="' + x + '" style="display:none;"></iframe>');
+    setTimeout(deleteIFrame(), 10000);
+    */
+}
